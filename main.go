63ad61ff9b36b592153ce52bc0aaaae04a2cc079
Acebond <Acebond>
2017-02-04 18:17:07 +1100

    Unit tests

--- main.go
+++ main.go
@@ -6,25 +6,41 @@ import (
 	"os"
 )
 
-func eval(text string) {
+func eval(text string) int {
 	lexer := newLexer(SpaceMap(text))
 	parser := newParser(lexer)
 	interpreter := Interpreter{parser}
 	result := interpreter.interpret()
-	fmt.Println(result)
+	return result.(int)
+}
+
+func unitTest() {
+	tests := []struct {
+		input string
+		ans   int
+	}{
+		{"5+5", 10},
+		{"5-5", 0},
+		{"7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)", 10},
+	}
+
+	for _, test := range tests {
+		if res := eval(test.input); res != test.ans {
+			fmt.Printf("%s Failed! Got %d instead of %d\n", test.input, res, test.ans)
+		}
+	}
+	fmt.Printf("Testing Complete\n")
 }
 
 func main() {
-	if len(os.Args) > 1 { // User input
-		for {
-			reader := bufio.NewReader(os.Stdin)
-			fmt.Print("Calc> ")
-			text, _ := reader.ReadString('\n')
-			eval(text)
-		}
+	if len(os.Args) > 1 {
+		unitTest()
+		return
 	}
-
-	text := "7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)"
-	eval(text)
-
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Calc> ")
+		text, _ := reader.ReadString('\n')
+		fmt.Println(eval(text))
+	}
 }
